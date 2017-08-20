783a98e0df58bf6044f48aab3a6a5bcb538d22d7
eleven-studio <31176131+eleven-studio@users.noreply.github.com>
2017-08-20 14:47:34 +0200

    Update printStuff.c

--- eleven-studio/portammurabi/source/printStuff.c
+++ eleven-studio/portammurabi/source/printStuff.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "headers.h"
-void printSomeContent(FILE* contentFile) // this function reads a file and prints its content
-{
+void printSomeContent(FILE* contentFile) // this function reads a file and prints its content{
     char artLine[MAX_INPUT_LENGTH] = "";
     while (fgets(artLine, MAX_INPUT_LENGTH, contentFile) != NULL){
         printf("%s", artLine);}
